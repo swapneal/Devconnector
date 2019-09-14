e72139cca69e23c23ff0b3ed0ad932200203f2fe
swapneal <52267392+swapneal@users.noreply.github.com>
2019-09-14 11:19:17 -0700

    user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test', (req,res) => res.json({
-  msg:'Inside Users test page'
-}));
+const User = require('../../models/User');
+
+// router.get('/test', (req,res) => res.json({
+//   msg:'Inside Users test page' - to verify route is in users page
+ 
+// }));
+
+// @route POST api/users/register
+// @desc Register a user
+// @access public route 
+router.post('/register', (req,res) => {
+  User.findOne({email: req.body.email})
+  .then(user => {
+    if(user) {
+      return res.status(400).json({   //using BAD request response code of 400, if status is not provided then executed code will be status 200
+        email: 'Email already exists'  //this email variable is unique from above email variable as this is for response
+      })
+    } else {
+     // return res.status(200).json({msg: 'success'});
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar
+      });
+    }
+  })
+  .catch(err => console.log('unique email check: '+err))
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
+//body parser
+const bodyParser = require('body-parser');
+
 //db config
 const db = require('./config/keys').mongoURI; //here we have import the only required key from config file
+
+const app = express(); //creating instance of express
 
 //connect to mongoDB  
 mongoose.connect(db).then(() => console.log('mongodb connection successful')).catch(err => console.log('mongodb connection failed: '+ err));
 
-//then catch is a promise statement
+//then-catch is a promise statement
 
-const app = express(); //creating instance of express
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false})); //using default form of encoding in the url
+app.use(bodyParser.json()); //using json while passing the data
+
 
 app.get('/', (req,res) => res.send('hello')); //creating first route - get route. '/' is home page, execute arrow function. Basically this is GET request
 
@@ -26,5 +35,4 @@ app.use('/api/posts', posts);
 const port = 5800; //create a port to make express to listen on this port
 
 app.listen(port, () => console.log(`server running on port ${port}`)); //express will listen on the port
-
 
