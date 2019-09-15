26c2f3bb6335cc96e77cdfd44c2dd2a7f5ae70b4
swapneal <52267392+swapneal@users.noreply.github.com>
2019-09-15 11:23:46 -0700

    Hashded password & Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 
 const router = express.Router();
+
+
 
 const User = require('../../models/User');
 
@@ -21,15 +26,50 @@ router.post('/register', (req,res) => {
       })
     } else {
      // return res.status(200).json({msg: 'success'});
+      
+      const avatar = gravatar.url(req.body.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      })
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar
+        password: req.body.password, //plain text password
+        avatar, //in JS, if variable name in LHS is same as RHS then you can just use a variable name, instead of avatar: avatar 
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) throw err;
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash; //hashed password
+          newUser.save().then( user => res.json(user)).catch(err => console.log('Error in saving hashed password: ') + err);
+        })  //
+      }); //callback function
+
     }
   })
   .catch(err => console.log('unique email check: '+err))
 });
+
+
+// @route POST api/users/login
+// @desc user login, return JWT token
+// @access public route 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({email}).then(user => {
+    if (!user){
+      return res.status(404).json({
+        msg: 'Email not found'
+      })
+    }
+  }).catch(err => console.log('Error in user email: '+ err));
+})
+
 
 module.exports = router;
