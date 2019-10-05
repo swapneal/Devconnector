5836d24d5d986f78194bffec6517fa7e1404861a
swapneal <52267392+swapneal@users.noreply.github.com>
2019-10-05 11:25:17 -0700

    Register, Login React Page

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer class="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Dev Connector
+    </footer>      
+  
   )
 }
+
